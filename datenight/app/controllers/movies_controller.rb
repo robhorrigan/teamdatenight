d21b513a0f858ec7374ee1cc4adf6a35cee8515e
Kate Brender <kate.brender@gmail.com>
2014-07-08 14:47:00 -0400

    movie gem installed, movies slightly parsed

--- datenight/app/controllers/movies_controller.rb
+++ datenight/app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
   # GET /movies
   # GET /movies.json
   def index
-    @movies = Movie.all
+    @movies = Fandango.movies_near(@zipcode)
   end
 
   # GET /movies/1
@@ -24,17 +24,17 @@ class MoviesController < ApplicationController
   # POST /movies
   # POST /movies.json
   def create
-    @movie = Movie.new(movie_params)
+    @movies = Fandango.movies_near(params[:zipcode])
 
-    respond_to do |format|
-      if @movie.save
-        format.html { redirect_to @movie, notice: 'Movie was successfully created.' }
-        format.json { render :show, status: :created, location: @movie }
-      else
-        format.html { render :new }
-        format.json { render json: @movie.errors, status: :unprocessable_entity }
-      end
-    end
+    # respond_to do |format|
+    #   if @movies.save
+    #     format.html { redirect_to @movie, notice: 'Movie was successfully created.' }
+    #     format.json { render :show, status: :created, location: @movie }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @movie.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /movies/1
@@ -69,6 +69,6 @@ class MoviesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def movie_params
-      params.require(:movie).permit(:name)
+      params.require(:zipcode)
     end
 end
