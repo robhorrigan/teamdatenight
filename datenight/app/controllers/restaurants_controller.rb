b534540d713c20fea7a1a76346ec58e0f747d66c
jasperknowsbest <edward.jasper@flatironschool.com>
2014-07-08 15:36:48 -0400

    Restaurants lists

--- datenight/app/controllers/restaurants_controller.rb
+++ datenight/app/controllers/restaurants_controller.rb
@@ -4,7 +4,8 @@ class RestaurantsController < ApplicationController
   # GET /restaurants
   # GET /restaurants.json
   def index
-    @restaurants = Restaurant.all
+     @restaurants = Restaurant.findings["businesses"]
+
   end
 
   # GET /restaurants/1
--- datenight/app/models/restaurant.rb
+++ datenight/app/models/restaurant.rb
@@ -1,17 +1,20 @@
 class Restaurant < ActiveRecord::Base
   belongs_to :movie
-  consumer_key = 'eTea0o4ChaOIb63F-te7Gg'
-  consumer_secret = 'eJgmD0A9xzA40JLP3WeZ_cxhAHQ'
-  token = 'BK68HjB4vIhIep_RYng-Drf9adE6FhvY'
-  token_secret = '2Fa0SGivQYnjgd7S3YtMelWwjUw'
 
-  api_host = 'api.yelp.com'
+  def self.findings
+    consumer_key = 'eTea0o4ChaOIb63F-te7Gg'
+    consumer_secret = 'eJgmD0A9xzA40JLP3WeZ_cxhAHQ'
+    token = 'BK68HjB4vIhIep_RYng-Drf9adE6FhvY'
+    token_secret = '2Fa0SGivQYnjgd7S3YtMelWwjUw'
 
-  consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
-  access_token = OAuth::AccessToken.new(consumer, token, token_secret)
+    api_host = 'api.yelp.com'
 
-  path = "/v2/search?term=restaurants&location=#{zip_code}&limit=20"
-  
-  restaurant_hash = JSON.parse(access_token.get(path).body)
-  
+    consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
+    access_token = OAuth::AccessToken.new(consumer, token, token_secret)
+
+    path = "/v2/search?term=restaurants&location=11106&limit=20"
+
+    JSON.parse(access_token.get(path).body)
+  end
+
 end
