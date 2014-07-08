712028a9afc4208b974c411715a01fd48f80431c
jasperknowsbest <edward.jasper@flatironschool.com>
2014-07-08 11:29:27 -0400

    Add API key info for Yelp

--- datenight/app/models/restaurant.rb
+++ datenight/app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ActiveRecord::Base
   belongs_to :movie
+		consumer_key = 'eTea0o4ChaOlb63F-te7Gg'
+		consumer_secret = 'eJgmD0A9xzA40JLP3WeZ_cxhAHQ'
+		token = 'BK68HjB4vlhlep_RYng-Drf9adE6FhvY'
+		token_secret = '2FaOSGivQYnjgd7S3YtMelWwjUw'
+
 end
