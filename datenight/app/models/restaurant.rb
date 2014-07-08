f9155ca344fb8446f3b1c068472063f8281aa342
jasperknowsbest <edward.jasper@flatironschool.com>
2014-07-08 12:13:46 -0400

    Verify API Keys

--- datenight/app/models/restaurant.rb
+++ datenight/app/models/restaurant.rb
@@ -1,8 +1,8 @@
 class Restaurant < ActiveRecord::Base
   belongs_to :movie
-		consumer_key = 'eTea0o4ChaOlb63F-te7Gg'
+		consumer_key = 'eTea0o4ChaOIb63F-te7Gg'
 		consumer_secret = 'eJgmD0A9xzA40JLP3WeZ_cxhAHQ'
-		token = 'BK68HjB4vlhlep_RYng-Drf9adE6FhvY'
-		token_secret = '2FaOSGivQYnjgd7S3YtMelWwjUw'
+		token = 'BK68HjB4vIhIep_RYng-Drf9adE6FhvY'
+		token_secret = '2Fa0SGivQYnjgd7S3YtMelWwjUw'
 
 end
