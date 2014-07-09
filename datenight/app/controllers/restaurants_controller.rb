4dc796cdfa703f06d9c31d7e60f52b19244087ee
Kate Brender <kate.brender@gmail.com>
2014-07-09 16:23:52 -0400

    new route

--- datenight/app/controllers/restaurants_controller.rb
+++ datenight/app/controllers/restaurants_controller.rb
@@ -65,7 +65,7 @@ class RestaurantsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_restaurant
-      # @restaurant = Restaurant.find(params[:id])
+      @restaurant = Restaurant.new(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
