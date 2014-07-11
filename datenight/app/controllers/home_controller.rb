f20bc4d29b717cb4c0b74433febeab662c5a13aa
Robert Horrigan <roberthorrigan@gmail.com>
2014-07-11 11:55:15 -0400

    Index Form will use dropdown to search by the day specified -TODO display the day of week on dropdown

--- datenight/app/controllers/home_controller.rb
+++ datenight/app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
+require 'date'
+
 class HomeController < ApplicationController
+  
   def index
-      
+    today = Date.today
+    tomorrow = today.next
+    @next_day = tomorrow.next
+    @last_day = @next_day.next
+
+
   end
 end
