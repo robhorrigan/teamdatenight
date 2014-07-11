0f5cb846c713c4b0fb365f82aafe9c6f04e829c2
christinebeaubrun <christinebeaubrun@gmail.com>
2014-07-11 12:35:59 -0400

    Change to home controller

--- datenight/app/controllers/home_controller.rb
+++ datenight/app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
-      
+    today = Date.today
+    tomorrow = today.next
+    next_day = tomorrow.next
+    @next_day.strftime('%A %d %b %Y')
+    last_day = next_day.next
+    @last_day.strftime('%A %d %b %Y')
   end
 end
