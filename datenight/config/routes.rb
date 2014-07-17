653b30db6607ed0397e6ddf534f0e2209ef4efd1
Robert Horrigan <roberthorrigan@gmail.com>
2014-07-17 15:18:13 -0400

    Added team photos to about view

--- datenight/config/routes.rb
+++ datenight/config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
 
   post '/restaurants/show' => 'restaurants#show'
 
+  get '/about' => 'about#index', as: "about"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
