f116360b68925a8f203ef768e94565bec066b1ae
Kate Brender <kate.brender@gmail.com>
2014-07-18 11:13:23 -0400

    you can now enter multiple phone numbers

--- datenight/app/models/confirmation.rb
+++ datenight/app/models/confirmation.rb
@@ -1,16 +1,18 @@
 class Confirmation < ActiveRecord::Base
-  def self.message(number, message)
-    account_sid = "AC139f154ed289df71588b6b4fe2ba6c4c"
-    auth_token = "c54502782441b734c29477e19268ba62"
-    client = Twilio::REST::Client.new account_sid, auth_token
+  def self.message(numbers, message)
+    numbers.each do |number|
+      account_sid = "AC139f154ed289df71588b6b4fe2ba6c4c"
+     auth_token = "c54502782441b734c29477e19268ba62"
+     client = Twilio::REST::Client.new account_sid, auth_token    
 
-    from = "+17187171011" # 4DaysOut Twilio number
+      from = "+17187171011" # 4DaysOut Twilio number
 
-    client.account.messages.create(
-      :from => from,
-      :to => number,
-      :body => message
-    )
-    puts "Sent message to #{number}"
+      client.account.messages.create(
+       :from => from,
+       :to => number,
+       :body => message
+     )
+      puts "Sent message to #{number}"
+    end
   end
 end
